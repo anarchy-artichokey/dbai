54750ae0968bd682d4042b2c9b0ae3def21a03de
Arturo Perez <perez.arturo2005@outlook.com>
2025-10-08 15:50:23 -0500

    config

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import discord
-import matplotlib
+from discord import app_commands
+from matplotlib import axes
 import json
 
 def readConfig():
     with open("./config.json") as configFile:
         return json.load(configFile)
 
-client = discord.Client(readConfig())
+myGuild = discord.Object(readConfig()["guild"]["id"])
 
-myGuild = discord.Object()
+class myClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents = intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild = myGuild)
+        await self.tree.sync(guild = myGuild)
+
+intents = discord.Intents.default()
+intents.members = True
+intents.guilds = True
+intents.message_content = True
+client = myClient(intents = intents)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+@client.tree.command()
+async def line_plot(interaction: discord.Interaction, function: str):
 
+    graph = ""
 
-client.run('your token here')
+    await interaction.response.send_message(graph)
+
+@client.tree.command()
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("pong")
+
+client.run(readConfig()["discord"]["api_key"])
